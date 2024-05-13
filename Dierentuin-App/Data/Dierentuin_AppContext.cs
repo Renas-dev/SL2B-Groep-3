5c1577af6c621c108e0981869084480c3cb967f6
DPCoderr <s1173932@student.windesheim.nl>
2024-05-13 20:07:36 +0200

    added dbcontext and scaffolding

---
+++ Dierentuin-App/Data/Dierentuin_AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dierentuin_App.Data
+{
+    public class Dierentuin_AppContext : DbContext
+    {
+        public Dierentuin_AppContext (DbContextOptions<Dierentuin_AppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animal { get; set; } = default!;
+    }
+}
---
+++ Dierentuin-App/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Animal
+{
+    [Key] // Primary Key
+    public int Id { get; set; }
+    public string AnimalRace { get; set; }
+    public string AnimalName { get; set; }
+    public bool IsAwake { get; set; }
+    public bool IsHungry { get; set; }
+    public string Environment { get; set; }
+}
+
+
