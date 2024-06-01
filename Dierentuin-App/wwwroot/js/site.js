5f5c7a7440056de7ab69eaec94fc69288b700088
Sam Mayer <sammayer0815@gmail.com>
2024-06-01 16:49:00 +0200

    fixed header bug caused by day night css and made it work another way that does not cause bug

--- Dierentuin-App/wwwroot/js/site.js
+++ Dierentuin-App/wwwroot/js/site.js
@@ -2,21 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document
-  .getElementById("theme-switcher")
-  .addEventListener("click", function () {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var switchToTheme = currentTheme === "night" ? "day" : "night";
-    document.documentElement.setAttribute("data-theme", switchToTheme);
-  });
-
-  document.getElementById('theme-switcher').addEventListener('click', function() {
-    $.ajax({
-      url: '/YourController/ToggleDayNight', // Replace with your controller and action
-      type: 'POST',
-      success: function(response) {
-        var theme = response.isDay ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', theme);
-      }
-    });
-  });
