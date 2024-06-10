f4fdc1867e36f037019bca23d5c5266b2aa8fb77
Sam Mayer <sammayer0815@gmail.com>
2024-06-10 16:41:47 +0200

    voorbeeld van api gemaakt

---
+++ Dierentuin-App/Controllers/MyApiController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Dierentuin_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyApiController : ControllerBase
+    {
+        // GET: api/<MyApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MyApiController>/5/tempValue
+        [HttpGet("{id}/{temp}")]
+        public string Get(int id, string temp)
+        {
+            return id.ToString() + ", " + temp;
+        }
+
+        // POST api/<MyApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MyApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MyApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
