27be73eb240f0b0998d42a166ee8a156b24375b6
Sam Mayer <sammayer0815@gmail.com>
2024-06-12 14:17:14 +0200

    failed auto generate api's

--- Dierentuin-App/Models/Animal.cs
+++ Dierentuin-App/Models/Animal.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Dierentuin_App.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
 
 public class Animal
 {
@@ -12,4 +16,66 @@ public class Animal
     public string Environment { get; set; }
 }
 
+
+public static class AnimalEndpoints
+{
+	public static void MapAnimalEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Animal").WithTags(nameof(Animal));
+
+        group.MapGet("/", async (Dierentuin_AppContext db) =>
+        {
+            return await db.Animal.ToListAsync();
+        })
+        .WithName("GetAllAnimals")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Animal>, NotFound>> (int id, Dierentuin_AppContext db) =>
+        {
+            return await db.Animal.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id)
+                is Animal model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetAnimalById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Animal animal, Dierentuin_AppContext db) =>
+        {
+            var affected = await db.Animal
+                .Where(model => model.Id == id)
+                .ExecuteUpdateAsync(setters => setters
+                  .SetProperty(m => m.Id, animal.Id)
+                  .SetProperty(m => m.AnimalRace, animal.AnimalRace)
+                  .SetProperty(m => m.AnimalName, animal.AnimalName)
+                  .SetProperty(m => m.IsAwake, animal.IsAwake)
+                  .SetProperty(m => m.IsHungry, animal.IsHungry)
+                  .SetProperty(m => m.Environment, animal.Environment)
+                  );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateAnimal")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Animal animal, Dierentuin_AppContext db) =>
+        {
+            db.Animal.Add(animal);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Animal/{animal.Id}",animal);
+        })
+        .WithName("CreateAnimal")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, Dierentuin_AppContext db) =>
+        {
+            var affected = await db.Animal
+                .Where(model => model.Id == id)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteAnimal")
+        .WithOpenApi();
+    }
+}
 
--- Dierentuin-App/Program.cs
+++ Dierentuin-App/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Dierentuin_App.Data;
+using Dierentuin_App.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Dierentuin_AppContext>(options =>
@@ -9,6 +10,10 @@ builder.Services.AddDbContext<Dierentuin_AppContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<Dierentuin_App.Services.DayNightService>();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,6 +34,8 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "stalls",
     pattern: "{controller=Stalls}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 // Make the application listen on port 80
 app.Run("http://*:8080");
