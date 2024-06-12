27fdec9e59fa77ca79dc8e59167367e4496d02fa
Sadek <sharkalmousawi@live.com>
2024-06-12 11:36:38 +0200

    Stalls backend done

---
+++ Dierentuin-App/Controllers/UpdatedStallsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Dierentuin_App.Data;
+using Dierentuin_App.Models;
+
+namespace Dierentuin_App.Controllers
+{
+    public class UpdatedStallsController : Controller
+    {
+        private readonly Dierentuin_AppContext _context;
+
+        public UpdatedStallsController(Dierentuin_AppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UpdatedStalls
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UpdatedStall.ToListAsync());
+        }
+
+        // GET: UpdatedStalls/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var updatedStall = await _context.UpdatedStall
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (updatedStall == null)
+            {
+                return NotFound();
+            }
+
+            return View(updatedStall);
+        }
+
+        // GET: UpdatedStalls/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UpdatedStalls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Climate,HabitatType,SecurityLevel,Size")] UpdatedStall updatedStall)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(updatedStall);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(updatedStall);
+        }
+
+        // GET: UpdatedStalls/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var updatedStall = await _context.UpdatedStall.FindAsync(id);
+            if (updatedStall == null)
+            {
+                return NotFound();
+            }
+            return View(updatedStall);
+        }
+
+        // POST: UpdatedStalls/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Climate,HabitatType,SecurityLevel,Size")] UpdatedStall updatedStall)
+        {
+            if (id != updatedStall.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(updatedStall);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UpdatedStallExists(updatedStall.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(updatedStall);
+        }
+
+        // GET: UpdatedStalls/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var updatedStall = await _context.UpdatedStall
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (updatedStall == null)
+            {
+                return NotFound();
+            }
+
+            return View(updatedStall);
+        }
+
+        // POST: UpdatedStalls/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var updatedStall = await _context.UpdatedStall.FindAsync(id);
+            if (updatedStall != null)
+            {
+                _context.UpdatedStall.Remove(updatedStall);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UpdatedStallExists(int id)
+        {
+            return _context.UpdatedStall.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Dierentuin-App/Migrations/20240611114514_CreateUpdatedStallTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dierentuin_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUpdatedStallTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UpdatedStall",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Climate = table.Column<string>(type: "TEXT", nullable: false),
+                    HabitatType = table.Column<string>(type: "TEXT", nullable: false),
+                    SecurityLevel = table.Column<string>(type: "TEXT", nullable: false),
+                    Size = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UpdatedStall", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UpdatedStall");
+        }
+    }
+}
