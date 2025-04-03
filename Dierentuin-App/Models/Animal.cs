af0133d0c67d0326f78486530fc421cde78789b7
Your Name <you@example.com>
2025-04-03 15:58:06 +0200

    Added creational pattern (factory method) for animals

--- Dierentuin-App/Models/Animal.cs
+++ Dierentuin-App/Models/Animal.cs
@@ -8,25 +8,36 @@ namespace Dierentuin_App.Models
     {
         [Key] // Primary Key
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Name is required")]
         public string? Name { get; set; }
+
         [Required(ErrorMessage = "Species is required")]
         public string? Species { get; set; }
+
         [Required(ErrorMessage = "Category is required")]
         public string? Category { get; set; }
+
         public AnimalSize Size { get; set; }
+
         public AnimalDietaryClass DietaryClass { get; set; }
+
         public AnimalActivityPattern ActivityPattern { get; set; }
+
         [Required(ErrorMessage = "Prey is required")]
         public string? Prey { get; set; }
+
         [Required(ErrorMessage = "Enclosure is required")]
         public string? Enclosure { get; set; }
+
         [Required(ErrorMessage = "Space requirment is required")]
         public double SpaceRequirement { get; set; }
+
         public AnimalSecurityRequirement SecurityRequirement { get; set; }
 
         // Foreign Key to Stall
         public int StallId { get; set; }
+
         public virtual Stall Stall { get; set; }
 
         public enum AnimalSize
